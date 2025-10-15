901547e0f1a06104c04dc7b3048ae6d401e38f73
CoderDLL <fsamusev11@gmail.com>
2025-10-15 09:19:55 +0700

    Добавлен проект мессенджера

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9McHoEDY-eJuuvBkk-capB6bgbqIfCbA",
+  authDomain: "parallaxchat-2f962.firebaseapp.com",
+  projectId: "parallaxchat-2f962",
+  storageBucket: "parallaxchat-2f962.firebasestorage.app",
+  messagingSenderId: "83172372331",
+  appId: "1:83172372331:web:17a6328c2d7a499eefa62e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
